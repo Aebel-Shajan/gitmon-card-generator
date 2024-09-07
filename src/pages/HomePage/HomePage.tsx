57eba3040ca3f256c838e9849cfa43d6a4685dfe
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-09-07 17:53:36 +0100

    Add template for homepage.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const HomePage = () => {
+  const [username, setUsername] = useState<string>("");
+
   return (
     <div>
       <h1>Gitmon Card Generator</h1>
+      <div>
+        <label>Enter your github username here:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <button>Generate</button>
+      </div>
     </div>
   );
 };
