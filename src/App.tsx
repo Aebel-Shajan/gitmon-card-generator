1534db4a60a7678faf603eb123fe9797b332cedb
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-09-07 17:22:57 +0100

    Install prettier, husky and lint-staged.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <div>
         <h1>Gitmon card creator</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
